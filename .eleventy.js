b1c2961dccb37fdc12f0d18d99043deb224ec4f7
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-11-01 12:10:17 +0100

    PSI Fix

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/css"); 
     eleventyConfig.addPassthroughCopy("src/assets/js"); 
     eleventyConfig.addPassthroughCopy("src/assets/img"); 
+    eleventyConfig.addPassthroughCopy("src/assets/fonts"); 
     eleventyConfig.addPassthroughCopy("src/content/works/img"); 
     eleventyConfig.addWatchTarget("src/assets/sass");
     
