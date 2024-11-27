01adeabab7d9223829ab0403e22cc1bb48e1404b
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-11-27 18:21:24 +0100

    Lazy blur images

--- .eleventy.js
+++ .eleventy.js
@@ -70,7 +70,7 @@ module.exports = function(eleventyConfig) {
     });
     
 
-    eleventyConfig.addNunjucksAsyncShortcode('workImage', async (src, alt) => {
+    eleventyConfig.addNunjucksAsyncShortcode('postImage', async (src, alt) => {
       if (!alt) {
           console.warn(`Missing 'alt' attribute for image: ${src}`);
           alt = 'Image';
@@ -81,8 +81,8 @@ module.exports = function(eleventyConfig) {
           stats = await Image(src, {
               widths: [25, 320, 640, 960, 1200, 1800],
               formats: ['jpeg', 'webp'],
-              urlPath: '/content/works/img/',
-              outputDir: './public/content/works/img/',
+              urlPath: '/content/posts/img/',
+              outputDir: './public/content/posts/img/',
           });
       } catch (error) {
           console.error(`Error processing image: ${src}`, error);
@@ -121,6 +121,58 @@ module.exports = function(eleventyConfig) {
           ${alt ? `<figcaption>${alt}</figcaption>` : ''}
         </figure>`;
   });
+
+  eleventyConfig.addNunjucksAsyncShortcode('workImage', async (src, alt) => {
+    if (!alt) {
+        console.warn(`Missing 'alt' attribute for image: ${src}`);
+        alt = 'Image';
+    }
+
+    let stats;
+    try {
+        stats = await Image(src, {
+            widths: [25, 320, 640, 960, 1200, 1800],
+            formats: ['jpeg', 'webp'],
+            urlPath: '/content/works/img/',
+            outputDir: './public/content/works/img/',
+        });
+    } catch (error) {
+        console.error(`Error processing image: ${src}`, error);
+        return `<div class="image-wrapper error">Image could not be processed: ${src}</div>`;
+    }
+
+    let lowestSrc = stats['jpeg'][0];
+    let largestSrc = stats['jpeg'][2];
+
+    const srcset = Object.keys(stats).reduce(
+        (acc, format) => ({
+            ...acc,
+            [format]: stats[format].map(curr => curr.srcset).join(', '),
+        }),
+        {},
+    );
+
+    const source = `<source type="image/webp" srcset="${srcset['webp']}">`;
+
+    const img = `<img
+        loading="lazy"
+        alt="${alt}"
+        src="${lowestSrc.url}"
+        sizes="(min-width: 1024px) 1024px, 100vw"
+        srcset="${srcset['jpeg']}"
+        width="${lowestSrc.width}"
+        height="${lowestSrc.height}">`;
+
+    return `
+      <figure class="image-wrapper blur-load">
+        <img class="placeholder" src="${lowestSrc.url}" loading="lazy" alt="Placeholder" width="${largestSrc.width}" height="${largestSrc.height}">
+        <picture>
+          ${source}
+          ${img}
+        </picture>
+        ${alt ? `<figcaption>${alt}</figcaption>` : ''}
+      </figure>`;
+});
   
     
     return {
